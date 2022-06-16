3f969f6208c72d11f9e2f9f89d03b1a5f55b785b
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 13:36:09 +0300

    Update target.py

--- target.py
+++ target.py
@@ -3,5 +3,5 @@ import random
 
 url = "https://raw.githubusercontent.com/Samuraianonweb/password/main/1_200.txt"
 id = random.randint(1,2)
-file_name = 'targets.txt'
+file_name = '1_200.txt'
 urllib.urlretrieve(url, filename)
