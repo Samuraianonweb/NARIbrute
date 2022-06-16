341b87a1376b8ae2c6c86fe54bd5732ebbcb4346
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 21:48:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,13 +7,6 @@ import os
 import threading
 import time
 
-with open("pass.txt", r) as f:
-
-         id = [l.strip() for l in fi]
- ​        ​fi​ ​=​ ​f​.​read​().​splitlines​() 
-
- ​for​ ​id ​in​ ​fi​: 
- ​        ​t​ ​=​ ​threading​.​Thread​(​​args​=​(​id​,​fi​)
- ​        ​t​.​start​() 
- ​        ​threads​.​append​(​t​) 
- ​        ​time​.​sleep​(​0.5​)
+with open('pass.txt') as f:
+    lines = f.readlines()
+print(lines)
