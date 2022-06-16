0d3141eca90bd31c396134bf33d8623283da5962
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 13:31:04 +0300

    Create target.py

--- target.py
+++ target.py
@@ -1,7 +1,7 @@
-import urllib.request
+import urllib
 import random
 
 url = "https://raw.githubusercontent.com/Samuraianonweb/password/main/'+id+'200'"
 id = random.randint(1,2)
 file_name = 'targets.txt'
-urllib.request.urlretrieve(url, filename)
+urllib.urlretrieve(url, filename)
