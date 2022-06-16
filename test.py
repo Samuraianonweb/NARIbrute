f063e8281d0266aa893d35036e0e2330dfaec081
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 20:02:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-import urllib2
-
-sock = urllib2.urlopen("http://diveintopython.org/") 
-htmlSource = sock.read()                            
-sock.close()                                        
-print (htmlSource)
+from urllib.request import urlopen
+html = urlopen("http://www.google.com/").read()
+print(html)
