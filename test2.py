6e331cf887152ad81286ebbcb967faea3051aebe
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 22:26:12 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,6 +6,4 @@ link = "https://raw.githubusercontent.com/Samuraianonweb/password/main/"
 f = urllib.request.urlopen(link)
 myfile = f.read()
 
-writeFileObj = open('pass.txt', 'wb')
-writeFileObj.write(myfile)
-writeFileObj.close()
+print(myfile)
