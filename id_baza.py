4219e2e20b3254f3d5df70c211e689ed7edc243e
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 18:54:30 +0300

    Update id_baza.py

--- id_baza.py
+++ id_baza.py
@@ -1,4 +1,4 @@
 import urllib
 
-id = input('Ведіть ID бази')
+id = input('Number Id')
 urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(id)+'_500.txt', 'baza'+str(id)+'.txt')
