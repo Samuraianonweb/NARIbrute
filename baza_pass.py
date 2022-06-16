92fb019c589ce2ac1672b7b1ab05ffd0fdfd53f2
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 15:50:38 +0300

    Update baza_pass.py

--- baza_pass.py
+++ baza_pass.py
@@ -1,5 +1,5 @@
 import urllib
 import random
 
-id = random.randint(1,3)
+id = random.randint(1,14)
 urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(id)+'_500.txt', 'pass.txt')
