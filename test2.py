5d5f068ddef794265fa8f3db22ae9cc2d25d6d91
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 23:06:33 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,6 +6,6 @@ link = "https://raw.githubusercontent.com/Samuraianonweb/password/main/1_500.txt
 page = urllib.request.urlopen(link)
 html = page.read()
 mystr = html.decode("utf8")
-link.close()
+page.close()
 print(mystr)
 
