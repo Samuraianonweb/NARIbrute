a1f474f98c831ac7209755a4754aa02745293bd9
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 21:17:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-
-
-
 with​ ​open​(​id, ​'r'​) ​as​ ​f​: 
  ​        ​fi​ ​=​ ​f​.​read​().​splitlines​() 
   
- ​for​ ​passwd​ ​in​ ​fi​: 
- ​        ​t​ ​=​ ​threading​.​Thread​(​target​=​bruter​, ​args​=​(​passwd​,​fi​)) 
+ ​for​ ​id ​in​ ​fi​: 
+ ​        ​t​ ​=​ ​threading​.​Thread​(​​args​=​(​id​,​fi​)
  ​        ​t​.​start​() 
  ​        ​threads​.​append​(​t​) 
  ​        ​time​.​sleep​(​0.5​)
