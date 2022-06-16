6e4f2af405e893e04d79e270ddf48b5c219c7db3
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 22:56:45 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,7 +6,4 @@ link = "https://raw.githubusercontent.com/Samuraianonweb/password/main/1_500.txt
 page = urllib.request.urlopen(link)
 html = page.read()
 
-with open(html) as f:
-    mas = f.read().splitlines()
-for x in mas:
-    print(html)
+print(html)
