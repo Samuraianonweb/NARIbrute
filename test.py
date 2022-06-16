d10067f10d38a0453818b2ecab3136b3b2ecafd3
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 22:12:41 +0300

    Create test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import urllib.request
 
-html = urllib.request.urlopen('https://raw.githubusercontent.com/Samuraianonweb/password/main/1_500.txt')
-
+url = "http://olympus.realpython.org/profiles/aphrodite"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
 with open(html) as f:
     mas = f.read().splitlines()
 for x in mas:
