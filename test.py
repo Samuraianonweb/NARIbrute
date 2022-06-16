4eedbb83bc7d00259928182315fcd0d80ad6b4de
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 20:24:52 +0300

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from urllib.request import urlopen
-passid = urlopen("https://raw.githubusercontent.com/Samuraianonweb/password/main/1_500.txt").read()
-parsid = passid.decode("utf-8")
-print(parsid)
