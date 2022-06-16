f37e921c2444d1165dff8cd646100067c6e10a84
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 19:48:59 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+from​ ​urllib​.​request 
+import​ ​urlopen
+id = "https://raw.githubusercontent.com/Samuraianonweb/password/main/all.txt"
+
+idpash = urlopen(id)
+passbit = idpash.read()
+passid = pashbit.decode("utf-8")
+print (passid)
