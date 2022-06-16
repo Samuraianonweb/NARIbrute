07fc1bdc202c3293d1ccf72e3b7f841adbffe514
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 21:49:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-
-import urllib
-import cookielib
-import sys
-import os
-import threading
-import time
-
 with open('pass.txt') as f:
     lines = f.readlines()
 print(lines)
