8e1ae5350a55da517def96e802c9370b0bf74632
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 19:55:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,4 @@ import​ ​urlopen
 id = "https://raw.githubusercontent.com/Samuraianonweb/password/main/all.txt"
 
 idpash = urlopen(id)
-passbit = idpash.read()
-passid = pashbit.decode("utf-8")
-print (passid)
+print(idpash)
