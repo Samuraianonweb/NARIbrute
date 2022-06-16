21fa3501762d2960a459e417f1ff32e19c0a77df
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 14:33:33 +0300

    Update pass.py

--- pass.py
+++ pass.py
@@ -2,4 +2,4 @@ import urllib
 import random
 
 id = random.randint(1,3)
-urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(id)+'_200.txt', 'test.txt')
+urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(id)+'_500.txt', 'pass.txt')
