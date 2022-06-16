0a34152f4c4c03509c58685bd55fc838751c3649
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-16 20:10:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from urllib.request import urlopen
 passid = urlopen("https://raw.githubusercontent.com/Samuraianonweb/password/main/1_500.txt").read()
-print(passid)
+parsid = passid.decode("utf-8")
+print(parsid)
