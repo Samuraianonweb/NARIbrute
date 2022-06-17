fa722a2c309f54d86565e11b60bac65aee21e19c
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-17 13:58:37 +0300

    Delete idnxe.py

--- idnxe.py
+++
@@ -1,8 +0,0 @@
-import urllib
-
-a=1
-while a < 58:
-    print(a)
-    a = a + 1
-
-urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(a)+'_500.txt', 'pass'+str(a)+'.txt')
