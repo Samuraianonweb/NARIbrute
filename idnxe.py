45ec5d3dd65e40f0d5a695f1d252564e56a18a54
SamuraiAnon <78055905+Samuraianonweb@users.noreply.github.com>
2022-06-17 13:56:38 +0300

    Update idnxe.py

--- idnxe.py
+++ idnxe.py
@@ -5,4 +5,4 @@ while a < 58:
     print(a)
     a = a + 1
 
-urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(a)+'_500.txt', 'pass.txt')
+urllib.urlretrieve('https://raw.githubusercontent.com/Samuraianonweb/password/main/'+str(a)+'_500.txt', 'pass'+str(a)+'.txt')
